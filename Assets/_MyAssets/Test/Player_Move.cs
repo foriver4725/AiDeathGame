a120659496c55186754b8a5c7418f821df5d14c2
MK6250 <tk230161@tks.iput.ac.jp>
2025-09-17 13:26:12 +0900

    MoveAI

---
+++ Assets/_MyAssets/Test/Player_Move.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class Player_Move : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private CharaAI nearbyCharacter;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Character"))
+        {
+            nearbyCharacter = other.GetComponent<CharaAI>();
+            if (nearbyCharacter != null)
+            {
+                Debug.Log($"キャラクター {nearbyCharacter.CharacterName} に近づきました。");
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Character"))
+        {
+            if (nearbyCharacter != null && nearbyCharacter.gameObject == other.gameObject)
+            {
+                Debug.Log($"キャラクター {nearbyCharacter.CharacterName} から離れました。");
+                nearbyCharacter = null;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(horizontal, vertical, 0);
+        transform.position += movement * moveSpeed * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.E) && nearbyCharacter != null)
+        {
+            MG.Instance.StartConversationWithCharacter(nearbyCharacter.CharacterAI);
+        }
+    }
+}
